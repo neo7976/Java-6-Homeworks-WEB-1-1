1912c427b9110e7b3498999095a3a70962ae2132
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-29 14:59:28 +0300

    Request.java - для обработки запросов

--- src/main/java/request/Request.java
+++ src/main/java/request/Request.java
@@ -1,4 +1,19 @@
 package request;
 
 public class Request {
+    private String method;
+    private String path;
+
+    public Request(String method, String path) {
+        this.method = method;
+        this.path = path;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
